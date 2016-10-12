d851e2dd71f528d178eb8c11f429e181065106a7
Mauricio Voto <mauriciovoto@gmail.com>
2016-10-12 20:28:03 -0300

    add note callback to possibly update project state
    - depending on note's content(if contains mark as `project.state`)

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,4 +9,21 @@ class Note < ApplicationRecord
 
   # Scopes
   default_scope { where(archived: false).order(created_at: :desc) }
+
+  # Callbacks
+  before_save :update_project_state, if: :update_project_via_content?
+
+  private
+  attr_reader :match_phrase
+
+  def update_project_via_content?
+    states = Project::STATES.collect{|state| "mark as #{state}" }.join("|")
+    @match_phrase = content.match(/(#{states})/)
+    match_phrase.present?
+  end
+
+  def update_project_state
+    project.state = match_phrase[0].split(' ').last
+    project.save
+  end
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -8,4 +8,25 @@ describe Note do
   context "Validations" do
     it { is_expected.to validate_presence_of(:content) }
   end
+
+  context "Callbacks" do
+    before { subject.save }
+
+    context 'given a content that should not update project state' do
+      subject { build(:note) }
+
+      it 'does not update project state' do
+        expect(subject.project.reload.state).to eq("started")
+      end
+    end
+
+    context 'given a content that is supposed to update project state' do
+      subject { build(:note, content: 'mark as completing') }
+
+      it 'updates project state as completing' do
+        subject.save
+        expect(subject.project.reload.state).to eq("completing")
+      end
+    end
+  end
 end
